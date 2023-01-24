9e22d661b7507ab5d7b7e329539e39fc6806f5f1
maximefremow <ex1kmx@yandex.ru>
2023-01-24 17:56:41 +0800

    step5.3

--- src/games/even.js
+++ src/games/even.js
@@ -9,13 +9,18 @@ const num = () => {
 const game = () => {
   const games = beginGame();
   for (let i = 0; i < 3; i += 1) {
-    const answer = readlineSync.question(`${'Question: '}${num()}${'\nYour answer: '}`);
-    if ((num() % 2 === 0 && answer === 'yes') || (num() % 2 !== 0 && answer === 'no')) {
+    const randomNum = num();
+    console.log(`Question: ${randomNum}`);
+    const answer = readlineSync.question(`${'Your answer: '}`);
+    if ((randomNum % 2 === 0 && answer === 'yes') || (randomNum % 2 !== 0 && answer === 'no')) {
       console.log('Correct!');
     } else if (answer === 'yes') {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${games}!`);
       return;
     } else if (answer === 'no') {
+      console.log(`${answer} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${games}!`);
+      return;
+    } else if (answer !== 'no' || answer !== 'yes') {
       console.log(`${answer} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${games}!`);
       return;
     }
